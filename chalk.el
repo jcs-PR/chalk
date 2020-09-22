39fcc1d9d65d8c8af2cd79111ea40feb65d6936b
JenChieh <jcs090218@gmail.com>
2020-09-22 16:49:56 +0800

    Fixed init format.

--- chalk.el
+++ chalk.el
@@ -1,17 +1,18 @@
-;;; chalk.el ---   -*- lexical-binding: t; -*-
+;;; chalk.el --- Log message cleanly with color on it  -*- lexical-binding: t; -*-
 
 ;; Copyright (C) 2020  Shen, Jen-Chieh
+;; Created date 2020-09-22 16:47:44
 
 ;; Author: Shen, Jen-Chieh <jcs090218@gmail.com>
-;; Maintainer: Shen, Jen-Chieh <jcs090218@gmail.com>
-;; URL: https://github.com/jcs-elpa/chalk
+;; Description: Log message cleanly with color on it.
+;; Keyword: color log message text string
 ;; Version: 0.0.1
 ;; Package-Requires: ((emacs "24.3"))
-;; Keywords:
+;; URL: https://github.com/jcs-elpa/chalk
 
-;; This file is not part of GNU Emacs.
+;; This file is NOT part of GNU Emacs.
 
-;; This program is free software: you can redistribute it and/or modify
+;; This program is free software; you can redistribute it and/or modify
 ;; it under the terms of the GNU General Public License as published by
 ;; the Free Software Foundation, either version 3 of the License, or
 ;; (at your option) any later version.
@@ -22,11 +23,11 @@
 ;; GNU General Public License for more details.
 
 ;; You should have received a copy of the GNU General Public License
-;; along with this program. If not, see <https://www.gnu.org/licenses/>.
+;; along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
 ;;; Commentary:
 ;;
-;;
+;; Log message cleanly with color on it.
 ;;
 
 ;;; Code:
